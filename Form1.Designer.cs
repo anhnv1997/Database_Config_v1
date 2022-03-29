a11fd6749c075fa157ea759b3f1a6cefd9e7407e
Flick <anhnv@kztek.net>
2022-03-29 23:23:09 +0700

    First Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿
+namespace ConnectionConfig
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtServerName = new System.Windows.Forms.TextBox();
+            this.cbAuthentication = new System.Windows.Forms.ComboBox();
+            this.txtLogin = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.cbDatabaseName = new System.Windows.Forms.ComboBox();
+            this.btnCheckConnection = new FontAwesome.Sharp.IconButton();
+            this.btnSave = new FontAwesome.Sharp.IconButton();
+            this.btnCancel = new FontAwesome.Sharp.IconButton();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Login";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 15);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Password";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 15);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Server Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 47);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 15);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Authentication";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(9, 134);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(90, 15);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Database Name";
+            // 
+            // txtServerName
+            // 
+            this.txtServerName.Location = new System.Drawing.Point(106, 13);
+            this.txtServerName.Name = "txtServerName";
+            this.txtServerName.Size = new System.Drawing.Size(190, 23);
+            this.txtServerName.TabIndex = 4;
+            // 
+            // cbAuthentication
+            // 
+            this.cbAuthentication.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbAuthentication.FormattingEnabled = true;
+            this.cbAuthentication.Items.AddRange(new object[] {
+            "SQL Server Authentication",
+            "Windows Authentication"});
+            this.cbAuthentication.Location = new System.Drawing.Point(106, 42);
+            this.cbAuthentication.Name = "cbAuthentication";
+            this.cbAuthentication.Size = new System.Drawing.Size(190, 23);
+            this.cbAuthentication.TabIndex = 5;
+            // 
+            // txtLogin
+            // 
+            this.txtLogin.Location = new System.Drawing.Point(106, 71);
+            this.txtLogin.Name = "txtLogin";
+            this.txtLogin.Size = new System.Drawing.Size(190, 23);
+            this.txtLogin.TabIndex = 6;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(106, 100);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(190, 23);
+            this.txtPassword.TabIndex = 7;
+            // 
+            // cbDatabaseName
+            // 
+            this.cbDatabaseName.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbDatabaseName.FormattingEnabled = true;
+            this.cbDatabaseName.Location = new System.Drawing.Point(106, 129);
+            this.cbDatabaseName.Name = "cbDatabaseName";
+            this.cbDatabaseName.Size = new System.Drawing.Size(190, 23);
+            this.cbDatabaseName.TabIndex = 8;
+            // 
+            // btnCheckConnection
+            // 
+            this.btnCheckConnection.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnCheckConnection.IconChar = FontAwesome.Sharp.IconChar.Check;
+            this.btnCheckConnection.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnCheckConnection.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnCheckConnection.IconSize = 36;
+            this.btnCheckConnection.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCheckConnection.Location = new System.Drawing.Point(317, 16);
+            this.btnCheckConnection.Name = "btnCheckConnection";
+            this.btnCheckConnection.Size = new System.Drawing.Size(180, 36);
+            this.btnCheckConnection.TabIndex = 9;
+            this.btnCheckConnection.Text = "Check Connection";
+            this.btnCheckConnection.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCheckConnection.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnCheckConnection.UseVisualStyleBackColor = true;
+            this.btnCheckConnection.Click += new System.EventHandler(this.btnCheckConnection_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnSave.IconChar = FontAwesome.Sharp.IconChar.Save;
+            this.btnSave.IconColor = System.Drawing.Color.ForestGreen;
+            this.btnSave.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnSave.IconSize = 36;
+            this.btnSave.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSave.Location = new System.Drawing.Point(317, 66);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(180, 36);
+            this.btnSave.TabIndex = 9;
+            this.btnSave.Text = "Save Config";
+            this.btnSave.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSave.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnCancel.IconChar = FontAwesome.Sharp.IconChar.WindowClose;
+            this.btnCancel.IconColor = System.Drawing.Color.Red;
+            this.btnCancel.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnCancel.IconSize = 36;
+            this.btnCancel.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCancel.Location = new System.Drawing.Point(317, 116);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(180, 36);
+            this.btnCancel.TabIndex = 9;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCancel.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(509, 168);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnCheckConnection);
+            this.Controls.Add(this.cbDatabaseName);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtLogin);
+            this.Controls.Add(this.cbAuthentication);
+            this.Controls.Add(this.txtServerName);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtServerName;
+        private System.Windows.Forms.ComboBox cbAuthentication;
+        private System.Windows.Forms.TextBox txtLogin;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.ComboBox cbDatabaseName;
+        private FontAwesome.Sharp.IconButton btnCheckConnection;
+        private FontAwesome.Sharp.IconButton btnSave;
+        private FontAwesome.Sharp.IconButton btnCancel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Configuration;
+using System.IO;
+using System.Data.SqlClient;
+using SQLConns;
+using FileXMLs;
+using CryptorEngines;
+
+namespace ConnectionConfig
+{
+    public partial class Form1 : Form
+    {
+        SQLConn[] sqls = null;
+        SqlConnection connection;
+        public Form1()
+        {
+            InitializeComponent();
+            try
+            {
+                if (File.Exists(Application.StartupPath + "\\SQLConn.xml"))
+                {
+                    FileXML.ReadXMLSQLConn(Application.StartupPath + "\\SQLConn.xml", ref sqls);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("frmConnectionConfig: " + ex.Message);
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (txtServerName.Text == "" || cbDatabaseName.Text == "" || txtLogin.Text == "")
+            {
+                MessageBox.Show("Các trường dữ liệu không được trống", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            List<SQLConn> sqlconnlist = new List<SQLConn>();
+            SQLConn sqlconn = null;
+            sqlconn = new SQLConn("", txtServerName.Text, cbDatabaseName.Text, txtLogin.Text, CryptorEngine.Encrypt(txtPassword.Text, true), cbAuthentication.Text);
+            sqlconnlist.Add(sqlconn);
+            sqls = new SQLConn[sqlconnlist.Count];
+            sqlconnlist.CopyTo(sqls);
+            FileXML.WriteXMLSQLConn(Application.StartupPath + @"\SQLConn.xml", sqls);
+            MessageBox.Show("Save Config Success", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnCheckConnection_Click(object sender, EventArgs e)
+        {
+            connection = new SqlConnection(GetConnectStr(txtServerName.Text,"master",cbAuthentication.Text,txtLogin.Text,txtPassword.Text));
+            try
+            {
+                connection.Open();
+                cbDatabaseName.Items.Clear();
+                SqlCommand cmd = connection.CreateCommand();
+                cmd.CommandText = "SELECT name from sys.databases";
+                SqlDataReader dataReader = cmd.ExecuteReader();
+                cbDatabaseName.Items.Clear();
+                cbDatabaseName.Items.Clear();
+                while (dataReader.Read())
+                {
+                    cbDatabaseName.Items.Add(dataReader["name"].ToString());
+                }
+                cbDatabaseName.SelectedIndex = 0;
+                btnSave.Enabled = true;
+                MessageBox.Show("Connect Success");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Connect to SQL SERVER FALSE: " + ex.Message);
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnSave.Enabled = false;
+            try
+            {
+                if (sqls != null && sqls.Length > 0)
+                {
+                    cbDatabaseName.Items.Add(sqls[0].SQLDatabase);
+                    txtServerName.Text = sqls[0].SQLServerName;
+                    cbDatabaseName.Text = sqls[0].SQLDatabase;
+                    txtLogin.Text = sqls[0].SQLUserName;
+                    txtPassword.Text = CryptorEngine.Decrypt(sqls[0].SQLPassword, true);
+                    cbAuthentication.Text = sqls[0].SQLAuthentication;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("frmConnectionConfig_Load: " + ex.Message);
+            }
+        }
+
+        private static bool IsServerConnected(SqlConnection connection, string connectionString)
+        {
+            connection = new SqlConnection(connectionString);
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        private string GetConnectStr(string serverName, string databaseName, string authentication, string username, string password)
+        {
+            if (authentication == "Window Authentication")
+            {
+                return $"data source={serverName};initial Catalog ={databaseName}; Integrated Security=True";
+
+            }
+            return $"data source={serverName};initial Catalog ={databaseName}; user id ={username};password={password}";
+        }
+    }
+}
